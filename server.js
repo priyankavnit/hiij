3258db0cb0d515c7f98bd73f10726c4ee2452de0
priyankavnit <priyancaparihar@gmail.com>
2015-03-03 16:12:03 +0530

    Hiij Second Update

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express           = require('express'),
     mongoose          = require('mongoose'),
     meetupsController = require('./server/controllers/meetups-controller');
 
-mongoose.connect('mongodb://localhost:27017/mean-demo');
-
+//mongoose.connect('mongodb://localhost:27017/mean-demo');
+mongoose.connect('mongodb://root:root@ds029950.mongolab.com:29950/mean-demo');
 app.use(bodyParser());
 
 app.get('/', function (req, res) {
@@ -18,6 +18,5 @@ app.use('/js', express.static(__dirname + '/client/js'));
 app.get('/api/meetups', meetupsController.list);
 app.post('/api/meetups', meetupsController.create);
 
-app.listen(3000, function() {
-  console.log('I\'m Listening...');
-})
+app.listen(process.env.PORT||5000) ;
+  console.log('I\'m Listening...at 5000');
